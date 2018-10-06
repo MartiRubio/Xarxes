08950905a46f55cd95cffdecd11bdff7b4cb90bd
TTinan <martirubioestrada@gmail.com>
2018-10-06 22:19:59 +0200

    tots fets menys Distance_calculator.c

--- P2/Connect_to_Wifi.c
+++ P2/Connect_to_Wifi.c
@@ -1,1 +1,31 @@
+void connectToWifi(void){
+    if(WiFi.status() != WL_CONNECTED){
+        WiFi.begin(c_ssid,c_pwd);
+        while(WiFi.status() != WL_CONNECTED){
+            delay(1000);
+            Serial.println("Connecting...");
+            contador++;
+            if(contador == 10){
+                break;
+            }
+        }
+        Serial.println(WiFi.localIP());
+    }
+}
 
+void wifiNetworkSelection(void){
+    if(WiFi.sttus() != WL_CONNECTED){
+        Serial.print("SSID? >");
+        while(!Serial.available());
+        ssid = Serial.readString();
+        Serial.print("SSID selected >");
+        Serial.println(ssid);
+        delay(1000);
+        Serial.print("Password? >");
+        while(!Serial.available());
+        password = Serial.readString();
+        Serial.print("Password >");
+        Serial.println(password);
+        delay(1000);
+    }
+}
--- P2/Scanner.c
+++ P2/Scanner.c
@@ -1,1 +1,31 @@
+#include "ESP8266WiFi.h"
 
+void setup(){
+    Serial.begin(9600);
+    WiFi.mode(WIFI_STA);
+    WiFi.disconnect();
+    delay(2000);
+    Serial.println("Setup done");
+}
+
+void loop(){
+    Serial.println("Scan start");
+    int n = WiFi.scanNetworks();
+    Serial.println("Scan done");
+    if(n == 0){
+        Serial.println("No networks found")
+    }
+    else{
+        for(i=0; i<n; i++){
+            Serial.print(i+1);
+            Serial.print(WiFi.SSID(i));
+            Serial.print("(");
+            Serial.print(WiFi.RSSI(i));
+            Serial.print(")");
+            Serial.println((WiFi.encryptionType(i) == ENC_TYPE_NONE)?" ":"*");
+            delay(10);
+        }
+    }
+    Serial.println("***************");
+    delay(50000);
+}
