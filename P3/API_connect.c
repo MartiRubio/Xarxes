e10cf22335dd552da2cb6909de3172690b1cde4b
TTinan <martirubioestrada@gmail.com>
2018-11-10 17:02:00 +0100

    API-connect acabat?

--- P3/API_connect.c
+++ P3/API_connect.c
@@ -5,11 +5,22 @@ const char* password = "t9rfepxq";
 
 WiFiClient client;
 
-const int channelID = XXX;
-String writeAPIKey = "F519ZAH6GDRI03JL";
+const int channelID = 623430;
+String writeAPIKey = "3DJ9KMQGRMNTYST0";
 
 const char* server = "api.thingspeak.com";
 const int postingInterval = 20*1000;
+
+// Codi de configuració
+void setup(){
+    Serial.begin(9600);
+    WiFi.mode(WIFI_STA);
+    WiFi.disconnect();
+    delay(2000);
+    Serial.println("Setup done");
+    wifiNetworkSelection();
+    connectToWifi();
+}
 
 void loop(){
     if(client.connect(server, 80)){
@@ -37,3 +48,36 @@ void loop(){
     delay(postingInterval);
 }
 
+// En aquest codi seleccionem el nom i la contrasenya de la xarxa a la que ens volem connectar.
+void wifiNetworkSelection(void){
+    if(WiFi.status() != WL_CONNECTED){
+        Serial.print("SSID? >");
+        while(!Serial.available());
+        ssid = Serial.readString();
+        Serial.print("SSID selected >");
+        Serial.println(ssid);
+        delay(1000);
+        Serial.print("Password? >");
+        while(!Serial.available());
+        password = Serial.readString();
+        Serial.print("Password >");
+        Serial.println(password);
+        delay(1000);
+    }
+}
+
+// En aquesta funció ens connectem a la xarxa amb els paràmetres desitjats.
+void connectToWifi(void){
+    if(WiFi.status() != WL_CONNECTED){
+        WiFi.begin(c_ssid,c_pwd);
+        while(WiFi.status() != WL_CONNECTED){
+            delay(1000);
+            Serial.println("Connecting...");
+            contador++;
+            if(contador == 10){
+                break;
+            }
+        }
+        Serial.println(WiFi.localIP());
+    }
+}
